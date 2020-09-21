671640b10c2df6f75224c3eb0f37dfbfe5673aaf
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-09-21 15:25:18 +0200

    Updates

---
+++ precache-manifest.fcda94fbe89025a8ce9ec7c4399792db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84445f54011dc4f107d9ad67050040c",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "15229fefca1cf02d9a3a",
+    "url": "/movie-search-app/static/css/main.d696eca6.chunk.css"
+  },
+  {
+    "revision": "55487868f32ab23d1714",
+    "url": "/movie-search-app/static/js/2.9a5a4a91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search-app/static/js/2.9a5a4a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15229fefca1cf02d9a3a",
+    "url": "/movie-search-app/static/js/main.ece7b1c0.chunk.js"
+  },
+  {
+    "revision": "8cb432894d9a7b81f07c",
+    "url": "/movie-search-app/static/js/runtime-main.f9515e68.js"
+  },
+  {
+    "revision": "7ad111eaef17f62489a8fe23aca1f19d",
+    "url": "/movie-search-app/static/media/tmdb.7ad111ea.svg"
+  }
+]);

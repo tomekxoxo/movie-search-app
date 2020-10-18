5ec1c63f38358b327b87da778dd7782506b8186c
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-10-18 18:10:36 +0200

    sidedrawer style change

--- src/components/Navigation/SideDrawer.js
+++ src/components/Navigation/SideDrawer.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import SearchBar from "./SearchBar";
-import SideDrawerButton from './SideDrawerButton';
+import SideDrawerButton from "./SideDrawerButton";
 
 const StyledDrawer = styled.div`
   width: 100vw;
@@ -16,25 +16,24 @@ const StyledDrawer = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  div:first-of-type{
-    align-self:flex-end;
-    margin:1rem 1rem 0 0;
+  div:first-of-type {
+    align-self: flex-end;
+    margin: 1rem 1rem 0 0;
   }
   div {
     @media screen and (max-width: 768px) {
       display: block !important;
-      order:2;
+      order: 2;
     }
   }
   ul {
+    margin-bottom: 12rem;
     @media screen and (max-width: 768px) {
-      order:3;
+      order: 3;
     }
-    
-    margin-bottom:5rem;
-    
+
     li {
-      margin-bottom:2rem;
+      margin-bottom: 2rem;
       a {
         font-size: 2rem;
       }
@@ -43,18 +42,16 @@ const StyledDrawer = styled.div`
 `;
 
 const SideDrawer = (props) => {
-
-
   return (
     <StyledDrawer>
-      <SideDrawerButton type="fas fa-times" clicked={props.toggleSideDrawer}/>
-      <SearchBar submitted={props.submitted} isOpen={props.isOpen}/>
+      <SideDrawerButton type="fas fa-times" clicked={props.toggleSideDrawer} />
+      <SearchBar submitted={props.submitted} isOpen={props.isOpen} />
       <ul>
         <li>
-          <NavLink to={process.env.PUBLIC_URL + '/movies'}>Filmy</NavLink>
+          <NavLink to={process.env.PUBLIC_URL + "/movies"}>Filmy</NavLink>
         </li>
         <li>
-          <NavLink to={process.env.PUBLIC_URL + '/series'}>Seriale</NavLink>
+          <NavLink to={process.env.PUBLIC_URL + "/series"}>Seriale</NavLink>
         </li>
       </ul>
     </StyledDrawer>
